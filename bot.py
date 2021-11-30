b2ec29fc90c8e2269cac3545fa808821eba56568
M4hbod <74229780+M4hbod@users.noreply.github.com>
2021-11-30 22:01:19 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ async def status_checker():
                 GET_CHANNEL_OR_GROUP = await app.get_chat(int(CHANNEL_OR_GROUP_ID))
                 CHANNEL_OR_GROUP_NAME = GET_CHANNEL_OR_GROUP.title
                 CHANNEL_OR_GROUP_TYPE = GET_CHANNEL_OR_GROUP.type
-                checker_bot = f"💡 **<u>LIVE BOT STATUS</u>** 💡\n\n💬 **{CHANNEL_OR_GROUP_NAME}**"
+                checker_bot = f"💡 **<u>وضعیت رباتون</u>** 💡\n\n💬 **{CHANNEL_OR_GROUP_NAME}**"
                 for bot in BOT_LIST:
                     try:
                         checker_status = await app.send_message(bot, "/start")
@@ -33,23 +33,23 @@ async def status_checker():
                         for ccc in checker_user:
                             bbb = ccc.message_id
                         if aaa == bbb:
-                            checker_bot += f"\n\n🤖 **BOT**: @{bot}\n🔴 **STATUS**: down ❌"
+                            checker_bot += f"\n\n🤖 **ربات**: @{bot}\n🔴 **وضعیت**: بگا ❌"
                             for bot_admin_id in BOT_ADMIN_IDS:
                                 try:
-                                    await app.send_message(int(bot_admin_id), f"🚨 **announcement** 🚨\n\n» @{bot} is down** ❌")
+                                    await app.send_message(int(bot_admin_id), f"🚨 **یالله** 🚨\n\n» ربات @{bot} بگا رفته** ❌")
                                 except Exception:
                                     pass
                             await app.read_history(bot)
                         else:
-                            checker_bot += f"\n\n🤖 **BOT**: @{bot}\n🟢 **STATUS**: alive ✅"
+                            checker_bot += f"\n\n🤖 **ربات**: @{bot}\n🟢 **وضعیت**: نابگا ✅"
                             await app.read_history(bot)
                     except FloodWait as e:
                         await asyncio.sleep(e.x)            
                 time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
                 last_update = time.strftime(f"%d %b %Y at %I:%M %p")
-                checker_bot += f"\n\n🛂 Last Check: {last_update} ({TIME_ZONE})\n\n🟡 **updates every 60 min(s)**\n\n⚡ __Powered by M4 Checker AI__"
+                checker_bot += f"\n\n🛂 آخرین چک: {last_update} ({TIME_ZONE})\n\n🟡 **هر 60 دقیقه چک خواهد جود**\n\n⚡ __قدرت گرفته از آق مخبذ__"
                 await app.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, checker_bot)
-                print(f"Last Check At: {last_update}")                
+                print(f"آخرین چک: {last_update}")                
                 await asyncio.sleep(3600)
                         
 app.run(status_checker())
